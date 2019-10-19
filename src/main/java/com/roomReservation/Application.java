e6779f76ff2757174fe19cfc063f0fa698ee9b72
VlacQ <swiech.kamil123@gmail,com>
2019-10-19 18:52:17 +0200

    Rooms Class

--- src/main/java/com/roomReservation/Application.java
+++ src/main/java/com/roomReservation/Application.java
@@ -1,7 +1,10 @@
 package com.roomReservation;
 
+import com.roomReservation.enumerate.Projector;
 import com.roomReservation.model.Employees;
+import com.roomReservation.model.Rooms;
 import com.roomReservation.service.EmployeesService;
+import com.roomReservation.service.RoomsService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +19,9 @@ public class Application {
     @Autowired
     private EmployeesService employeesService;
 
+    @Autowired
+    private RoomsService roomsService;
+
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
@@ -25,6 +31,9 @@ public class Application {
         return () -> {
             employeesService.createEmployee(new Employees("John", "Smith", "jsmith", "qwerty"));
             employeesService.createEmployee(new Employees("Jane", "Doe", "jdoe", "mySecret"));
+            roomsService.createRoom(new Rooms("Large Room", "1st floor", 10, Projector.YES, "22-22-22-22"));
+            roomsService.createRoom(new Rooms("Medium Room", "1st floor", 6, Projector.YES, ""));
+            roomsService.createRoom(new Rooms("Small Room", "2nd floor", 4, Projector.NO, ""));
         };
     }
 }
